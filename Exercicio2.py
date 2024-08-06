8130d2e53c9844d5a2a983203fdcd120da1a4e54
FilipiMiranda <filipi.benedito12@gmail.com>
2024-08-06 10:52:35 -0300

    Add files via upload

---
+++ Exercicio2.py
@@ -0,0 +1,7 @@
+Base = float(input("digite a Base do retangulo: "))
+Altura = float(input("digite a altura do retangulo: "))
+calculo = float(input(Base * Altura))
+
+Area = float(input("A area é de: " + calculo))
+
+
